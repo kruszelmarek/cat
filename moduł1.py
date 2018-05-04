d9952a04bbe3c9a24f8126cd590792ed11b68a08
Warzywko <Warzywko@DESKTOP-IL9341J>
2018-05-04 12:36:00 +0200

    argparse

--- moduł1.py
+++ moduł1.py
@@ -1,43 +1,46 @@
-import os;
-import argparse;
+import argparse, os, sys
 
-c = input(">>");
-ct = [];
-ct = c.split(" ");
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="name of file")
+option = parser.add_mutually_exclusive_group();
+option.add_argument('-n', '--numer', action='store_true', help='number all output lines')
+option.add_argument('-s', '--squeeze_blank', action='store_true', help='suppress repeated empty output lines')
+option.add_argument('-E', '--show_ends', action='store_true', help='display $ at end of each line')
+args = parser.parse_args()
 
-print("You entered: >>" + c);
+print("We will check in: " + os.path.dirname(os.path.abspath(__file__)));
 
-if(ct[0] == "cat"):
-    print("We will check in: " + os.path.dirname(os.path.abspath(__file__)));
-    if(os.path.exists(ct[2])):
-        f = open(ct[2], "r");
-        l = f.read().split("\n");
-        if(len(ct) == 2):
-            n = 0;
-            for i in l:
+file = args.file
+
+if(os.path.exists(file)):
+    try:
+        f = open(file, "r");
+    except:
+        print("Permission denied")
+        sys.exit(0)
+    l = f.read().split("\n");
+    if args.numer:
+        n = 0;
+        for i in l:
+            print(str(n) + " " + i);
+            n += 1;
+    elif args.squeeze_blank:
+        n = 0;
+        for i in l:
+            if(i == ""):
+                continue
+            else:
                 print(i);
                 n += 1;
-        elif(ct[1] == "-n"):
-            n = 0;
-            for i in l:
-                print(str(n) + " " + i);
-                n += 1;
-        elif(ct[1] == "-s"):
-            n = 0;
-            for i in l:
-                if(i == ""):
-                    continue
-                else:
-                    print(i);
+    elif args.show_ends:
+        n = 0;
+        for i in l:
+            print(i + "$");
             n += 1;
-        elif(ct[1] == "-E"):
-            n = 0;
-            for i in l:
-                print(i + "$");
-            n += 1;
-        else:
-            print("Enter correct argument");
     else:
-        print("File not exist!");
+        n = 0;
+        for i in l:
+            print(i);
+            n += 1;
 else:
-   print("Enter correct command!");
+    print("File not exists")
